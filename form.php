69d7c26b714f9868eb9ce45b1146038b43ca5a85
FernandoGlez <ing_omarglez@hotmail.com>
2016-08-17 18:30:08 -0500

    Create form.php

---
+++ form.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html xmlns="htttp://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Documento sin titulo</title>
+</head>
+
+<body>
+<form action="verificar.php" method="post">
+	<input type="text" name="user" /><br /><br />
+	<input type="password" name="pw" /><br /><br />
+	<input type="submit" value="Ingresar" />
+</form>
+</body>
+</html>
