93b86916ba2a5695d26d54b973e541e3152642f0
FernandoGlez <ing_omarglez@hotmail.com>
2016-08-17 18:31:25 -0500

    Create formIngreso.php

---
+++ formIngreso.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Documento sin titulo</title>
+</head>
+<body>
+<form action="verificar.php" method="post">
+	<input type="text" name="user" /><br /><br />
+	<input type="password" name="pw"><br /><br />
+	<input type="submit" value="Ingresar" />
+</form>
+</body>
+</html>
